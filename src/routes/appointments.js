b492edbf7b422e4d049c2f2e805655dcb236d4f2
javascriptsucks <evix1988@live.cn>
2023-03-01 13:54:55 -0600

    setup-for-cypress-test

--- src/routes/appointments.js
+++ src/routes/appointments.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
 
 module.exports = (db, updateAppointment) => {
-  router.get("/appointments", (request, response) => {
-    db.query(
-      `
+	router.get("/appointments", (request, response) => {
+		db.query(
+			`
       SELECT
         appointments.id,
         appointments.time,
@@ -16,56 +16,59 @@ module.exports = (db, updateAppointment) => {
       GROUP BY appointments.id, interviews.id, interviews.student, interviews.interviewer_id
       ORDER BY appointments.id
     `
-    ).then(({ rows: appointments }) => {
-      response.json(
-        appointments.reduce(
-          (previous, current) => ({ ...previous, [current.id]: current }),
-          {}
-        )
-      );
-    });
-  });
+		).then(({ rows: appointments }) => {
+			response.json(
+				appointments.reduce(
+					(previous, current) => ({ ...previous, [current.id]: current }),
+					{}
+				)
+			);
+		});
+	});
 
-  router.put("/appointments/:id", (request, response) => {
-    if (process.env.TEST_ERROR) {
-      setTimeout(() => response.status(500).json({}), 1000);
-      return;
-    }
+	console.log("Server connected");
 
-    const { student, interviewer } = request.body.interview;
+	router.put("/appointments/:id", (request, response) => {
+		if (process.env.TEST_ERROR) {
+			setTimeout(() => response.status(500).json({}), 1000);
+			return;
+		}
 
-    db.query(
-      `
+		const { student, interviewer } = request.body.interview;
+		db.query(
+			`
       INSERT INTO interviews (student, interviewer_id, appointment_id) VALUES ($1::text, $2::integer, $3::integer)
       ON CONFLICT (appointment_id) DO
       UPDATE SET student = $1::text, interviewer_id = $2::integer
     `,
-      [student, interviewer, Number(request.params.id)]
-    )
-      .then(() => {
-        setTimeout(() => {
-          response.status(204).json({});
-          updateAppointment(Number(request.params.id), request.body.interview);
-        }, 1000);
-      })
-      .catch(error => console.log(error));
-  });
+			[student, interviewer, Number(request.params.id)]
+		)
+			.then(() => {
+				setTimeout(() => {
+					response.status(204).json({});
+					updateAppointment(Number(request.params.id), request.body.interview);
+				}, 1000);
+			})
+			.catch((error) => console.log(error));
+	});
 
-  router.delete("/appointments/:id", (request, response) => {
-    if (process.env.TEST_ERROR) {
-      setTimeout(() => response.status(500).json({}), 1000);
-      return;
-    }
+	router.delete("/appointments/:id", (request, response) => {
+		if (process.env.TEST_ERROR) {
+			setTimeout(() => response.status(500).json({}), 1000);
+			return;
+		}
 
-    db.query(`DELETE FROM interviews WHERE appointment_id = $1::integer`, [
-      request.params.id
-    ]).then(() => {
-      setTimeout(() => {
-        response.status(204).json({});
-        updateAppointment(Number(request.params.id), null);
-      }, 1000);
-    });
-  });
+		db.query(
+			`DELETE FROM interviews WHERE appointment_id = $1::integer
+    `,
+			[request.params.id]
+		).then((res) => {
+			setTimeout(() => {
+				response.status(204).json({ res });
+				updateAppointment(Number(request.params.id), null);
+			}, 1000);
+		});
+	});
 
-  return router;
+	return router;
 };
